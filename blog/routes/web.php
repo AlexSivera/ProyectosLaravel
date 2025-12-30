b1deb89035fdf2932c7b3f81f0fe62e217532100
AlexSivera <alex@example.com>
2025-12-30 12:38:01 +0100

    Ejercicio1

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -4,10 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('inicio');
 
 
-// Nueva ruta para posts
+// Ruta para posts
 Route::get('/posts', function () {
     return 'Listado de posts';
-});
+})->name('posts_listado');
+
+// Ruta para ficha de post (Ejercicio 1)
+Route::get('/posts/{id}', function ($id) {
+    return "Ficha del post $id";
+})->where('id', '[0-9]+')->name('posts_ficha');
