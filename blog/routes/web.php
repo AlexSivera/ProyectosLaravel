d24a52e41863fd9886ae134ecfafb794d4d1e184
AlexSivera <alex@example.com>
2025-12-30 12:00:54 +0100

    Primer commit de ProyectosLaravel

---
+++ blog/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Nueva ruta para posts
+Route::get('/posts', function () {
+    return 'Listado de posts';
+});
